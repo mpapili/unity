b5fb16b78ed60baf8f1de6759933f910800a43d6
mpapili <michaelpapili@gmail.com>
2017-10-17 03:52:11 -0400

    next session! MOVING OBJECTS

---
+++ 2nd3dunitygame/unity files/2nd Unity Game/Assets/Scripts/objectsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectsScript : MonoBehaviour {
+
+	//Makes a private variable that the "inspector" in unity can see
+	[SerializeField] float objectSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//ACCESS ASSIGNED OBJECT'S TRANSFORM COMPONENT
+		//MOVE LEFT 20 * [time since last frame]
+		transform.Translate(Vector3.right * (objectSpeed * Time.deltaTime));
+	}
+}
